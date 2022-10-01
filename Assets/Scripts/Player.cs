6904c5b4b214cfb525c5ac431352c72eaeed94fb
ncumplido <ncumplido@msn.com>
2022-10-01 20:12:38 +0100

    Add project files.

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public float moveSpeed;
+    public float jumpForce;
+    public Rigidbody rig;
+    private bool isGrounded;
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Get x and z inputs
+        float x = Input.GetAxis("Horizontal") * moveSpeed;//A & D or left arrow and right arrow movement
+        float z = Input.GetAxis("Vertical") * moveSpeed;
+
+        //Set the velocity based on our inputs
+        rig.velocity = new Vector3(x, rig.velocity.y, z);
+
+        //This is to prevent the character from falling forward
+        Vector3 vel = rig.velocity;
+        vel.y = 0;
+
+        //Only look in the direction that we are moving when the character is moving
+        if(vel.x != 0 || vel.z != 0)
+        {
+            transform.forward = vel;
+        }
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            rig.AddForce(Vector3.up * jumpForce, ForceMode.Impulse); //I'm only really going to need Force and impulse (Force = gradual force, Impulse = instant velocity)
+        }
+    }
+}
