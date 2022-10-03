542707ef8a9f06a6fabbf7b0d714c319ea4ea638
ncumplido <ncumplido@msn.com>
2022-10-03 18:52:42 +0100

    Implement enemy movement

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    public float speed;
+    public Vector3 moveDirection;
+    public float moveDistance;
+    private Vector3 startPosition;
+    private bool movingToStart;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        startPosition = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (movingToStart)
+        {
+            transform.position = Vector3.MoveTowards(transform.position, startPosition, speed * Time.deltaTime);
+
+            if(transform.position == startPosition)
+            {
+                movingToStart = false;
+            }
+        }
+        else
+        {
+            transform.position = Vector3.MoveTowards(transform.position, startPosition + (moveDirection * moveDistance), speed * Time.deltaTime);
+
+            if (transform.position == startPosition + (moveDirection * moveDistance))
+            {
+                movingToStart = true;
+            }
+        }
+    }
+}
