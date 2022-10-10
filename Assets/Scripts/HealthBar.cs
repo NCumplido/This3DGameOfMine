53ee0c38ab0b97ae82e4317793dd39df2e06fe6e
ncumplido <ncumplido@msn.com>
2022-10-10 20:25:26 +0100

    Finish healthbar by adding fixed gradient

--- Assets/Scripts/HealthBar.cs
+++ Assets/Scripts/HealthBar.cs
@@ -6,14 +6,19 @@ using UnityEngine.UI;
 public class HealthBar : MonoBehaviour
 {
     public Slider sldrHealthFill;
+    public Gradient gradient;
+    public Image fill;
     public void SetMaxHealth(int health)
     {
         sldrHealthFill.maxValue = health;
         sldrHealthFill.value = health;
+
+        fill.color = gradient.Evaluate(1f);
     }
 
     public void SetHealth(int health)
     {
         sldrHealthFill.value = health;
+        fill.color = gradient.Evaluate(sldrHealthFill.normalizedValue);
     }
 }
